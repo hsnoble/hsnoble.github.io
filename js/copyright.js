f503a80741e691115578e045f94286f13694203a
Stephen Noble <stephen.noble0514@gmail.com>
2025-04-15 00:03:12 -0400

    reorganizing structure

---
+++ js/copyright.js
@@ -0,0 +1,9 @@
+document.write("<footer>" + 
+        "<div class='container'>" + 
+            "<div class='row'>" +
+                "<div class='col-md-5'>" +
+                "    <span class='copyright'>Copyright &copy; Stephen Noble | 2017 | <a href='https://startbootstrap.com/'>Created Using Start Bootstrap</a></span>" +
+                "</div>" +
+            "</div>" +
+        "</div>" +
+    "</footer>");
